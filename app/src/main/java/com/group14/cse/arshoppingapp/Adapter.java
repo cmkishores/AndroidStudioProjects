42172d928abe3c379071315c36a7471d1025a52e
anandtp <anandtp00@gmail.com>
2019-03-17 17:22:47 +0530

    changes
    Changes in Adapter java class
    changes in java file of Home

--- app/src/main/java/com/group14/cse/arshoppingapp/Adapter.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/Adapter.java
@@ -8,4 +8,44 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 public class Adapter extends BaseAdapter {
+    private Context mContext;
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.model1, R.drawable.model2,
+            R.drawable.model3, R.drawable.model4,
+            R.drawable.model5, R.drawable.model6,
+            R.drawable.model7, R.drawable.model8,
+
+    };
+
+    // Constructor
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+        return imageView;
+    }
+
 }
--- app/src/main/java/com/group14/cse/arshoppingapp/Home.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/Home.java
@@ -1,17 +1,14 @@
 package com.group14.cse.arshoppingapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
-public class Home extends AppCompatActivity {
+public class Home extends Activity {
 
     private TextView mTextMessage;
 
@@ -35,6 +32,16 @@ public class Home extends AppCompatActivity {
             return false;
         }
     };
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        GridView gridView = (GridView) findViewById(R.id.GridView);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new Adapter(this));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
