d17de437ef6d42ad225bc78c49c2540376cd67d6
anandtp <anandtp00@gmail.com>
2019-03-17 17:12:05 +0530

    Update Adapter.java

--- app/src/main/java/com/group14/cse/arshoppingapp/Adapter.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/Adapter.java
@@ -1,4 +1,11 @@
 package com.group14.cse.arshoppingapp;
 
-public class Adapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class Adapter extends BaseAdapter {
 }
