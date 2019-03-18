a132ffb77418e89075ffe53382086b84f7529f7e
CM Kishore <cmkishores@gmail.com>
2019-03-18 12:04:00 +0530

    Update ARFRAG.java

--- app/src/main/java/com/group14/cse/arshoppingapp/ARFRAG.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/ARFRAG.java
@@ -16,16 +16,19 @@ import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-public class ARFRAG extends AppCompatActivity {
+public class ARFRAG extends AppCompatActivity
+ {
     private ArFragment arFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arfrag);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
         assert arFragment != null;
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) ->
+        {
             Anchor anchor = hitResult.createAnchor();
             ModelRenderable.builder().setSource(ARFRAG.this, Uri.parse("model.sfb")).build().thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                     .exceptionally(throwable ->
@@ -36,11 +39,12 @@ public class ARFRAG extends AppCompatActivity {
                         return null;
                     });
         });
-                Intent intent = new Intent(ARFRAG.this, Home.class);
+                Intent intent = new Intent(ARFRAG.this, ARFRAG.class);
         startActivity(intent);
     }
 
-    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable)
+    {
         AnchorNode anchorNode = new AnchorNode(anchor);
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
         transformableNode.setParent(anchorNode);
@@ -48,4 +52,4 @@ public class ARFRAG extends AppCompatActivity {
         arFragment.getArSceneView().getScene().addChild(anchorNode);
         transformableNode.select();
     }
-}
+ }
