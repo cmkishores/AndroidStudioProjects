6a662b43f5695be7de3bd0edb344a5565abbb811
CM Kishore <cmkishores@gmail.com>
2019-04-05 11:25:21 +0530

    Remove unwanted code

--- app/src/main/java/com/group14/cse/arshoppingapp/Home.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/Home.java
@@ -14,18 +14,6 @@ import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.web3j.crypto.Credentials;
-import org.web3j.crypto.ECKeyPair;
-import org.web3j.crypto.WalletUtils;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
-import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.Transfer;
-import org.web3j.utils.Convert;
-
-import java.io.File;
-import java.math.BigDecimal;
 
 
 public class Home extends Activity
