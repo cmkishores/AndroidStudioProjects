aef1e2d39180b574cc037038e7a2ed212ec41442
anandtp <anandtp00@gmail.com>
2019-04-07 20:01:57 +0530

    changes

--- app/src/main/java/com/group14/cse/arshoppingapp/Home.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/Home.java
@@ -1,47 +1,28 @@
 package com.group14.cse.arshoppingapp;
-
-
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
 
-
-public class Home extends AppCompatActivity
-{
-   private String[] labels;
-   private int[] images;
-    private TextView mTextMessage;
-
-
+    String[] labels = {"Couch","Table","Bookcase","Flower Vase","Flower Vase","Television","Chair,Light Stand"};
+    int[] modelimages = {R.drawable.model1,R.drawable.model2,R.drawable.model3,R.drawable.model4,R.drawable.model5,R.drawable.model6,R.drawable.model7,R.drawable.model8};
+    String[] prize = {"10eth","7eth","8eth","4eth","4.5eth","7eth","6eth","5eth"}
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Resources res = getResources();
-        images = res.getIntArray(R.array.image);
-        labels = res.getStringArray(R.array.headings);
-        mTextMessage = (TextView) findViewById(R.id.message);
-
-
-        GridView gridView = (GridView) findViewById(R.id.GridView);
-
-        // Instance of ImageAdapter Class
+        //finding listview
+        gridView = findViewById(R.id.gridview);
 
         CustomAdapter customAdapter = new CustomAdapter();
         gridView.setAdapter(customAdapter);
@@ -51,7 +32,8 @@ public class Home extends AppCompatActivity
 //                Toast.makeText(getApplicationContext(),fruitNames[i],Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getApplicationContext(),ProductActivity.class);
                 intent.putExtra("name",labels[i]);
-                intent.putExtra("image",images[i]);
+                intent.putExtra("image",modelimages[i]);
+                intent.putExtra("prize",prize[i]);
                 startActivity(intent);
 
             }
@@ -63,7 +45,7 @@ public class Home extends AppCompatActivity
     private class CustomAdapter extends BaseAdapter {
         @Override
         public int getCount() {
-            return images.length;
+            return modelimages.length;
         }
 
         @Override
@@ -80,11 +62,12 @@ public class Home extends AppCompatActivity
         public View getView(int i, View view, ViewGroup viewGroup) {
             View view1 = getLayoutInflater().inflate(R.layout.grid_item,null);
             //getting view in row_data
-            TextView name = view1.findViewById(R.id.grid_text);
-            ImageView image = view1.findViewById(R.id.grid_image);
+            TextView name = view1.findViewById(R.id.label);
+            TextView prize = view1.findViewById(R.id.prize);
+            ImageView image = view1.findViewById(R.id.model_images);
 
-            name.setText(labels[i]);
-            image.setImageResource(images[i]);
+            name.setText(modelimages[i]);
+            image.setImageResource(modelimages[i]);
             return view1;
 
 
