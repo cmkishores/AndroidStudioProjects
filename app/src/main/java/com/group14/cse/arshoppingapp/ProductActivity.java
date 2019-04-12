e10c761e36bfede6650d3517a57017b9e81e43df
CM Kishore <cmkishores@gmail.com>
2019-04-12 13:31:30 +0530

    Update ProductActivity.java

--- app/src/main/java/com/group14/cse/arshoppingapp/ProductActivity.java
+++ app/src/main/java/com/group14/cse/arshoppingapp/ProductActivity.java
@@ -41,63 +41,62 @@ public class ProductActivity extends AppCompatActivity
     ImageView imageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
-       walletPath = getFilesDir().getAbsolutePath();
-        walletDir  = new File(walletPath);
+        walletPath = getFilesDir().getAbsolutePath();
+        walletDir = new File(walletPath);
 
         gridData = findViewById(R.id.gridData);
         imageView = findViewById(R.id.imageView);
         Intent intent1 = getIntent();
-        String receivedName =  intent1.getStringExtra("name");
-        int receivedImage = intent1.getIntExtra("image",0);
+        String receivedName = intent1.getStringExtra("name");
+        int receivedImage = intent1.getIntExtra("image", 0);
         gridData.setText(receivedName);
         imageView.setImageResource(receivedImage);
 //29cf16c50a8ecf3a4ccfcb9f678b51d0410f33bf4e6edb916b701f8396189e60
-        btn2=(Button)findViewById(R.id.BuyButton3D);
-        btn1=(Button)findViewById(R.id.buybutton);
+        btn2 = (Button) findViewById(R.id.BuyButton3D);
+        btn1 = (Button) findViewById(R.id.buybutton);
 
         btn2.setOnClickListener(v -> {
-            if(receivedName .equals("Couch - 10eth")) {
+            if (receivedName.equals("Couch - 10eth")) {
                 Intent intent = new Intent(ProductActivity.this, ARFRAG.class);
                 startActivity(intent);
-            }
-            else if (receivedName .equals("Book Case - 8eth")) {
+            } else if (receivedName.equals("Book Case - 8eth")) {
                 Intent intent = new Intent(ProductActivity.this, ARFRAG3.class);
                 startActivity(intent);
-            }
-            else if (receivedName .equals("Flower Vase - 5eth")) {
+            } else if (receivedName.equals("Flower Vase - 5eth")) {
                 Intent intent = new Intent(ProductActivity.this, ARFRAG4.class);
                 startActivity(intent);
-            }
-            else if (receivedName .equals("Flower Vase - 4eth")) {
+            } else if (receivedName.equals("Flower Vase - 4eth")) {
                 Intent intent = new Intent(ProductActivity.this, ARFRAG5.class);
                 startActivity(intent);
             }
         });
 
+
         btn1.setOnClickListener(v ->
         {
            t= connectToEthNetwork(v);
 
-           toastAsync("Wallet generated");
-            if(t==true) {
+
+            if(t==true)
+            {
 
 
             toastAsync("Wallet generated");
 
 
                 createWallet(v);
+                toastAsync("Wallet generated");
                 getAddress(v);
                 sendTransaction(v);
 
 
-        })
+        }
+    });
     }
-
 
     public boolean connectToEthNetwork(View v)
     {
